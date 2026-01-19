8ec80f43e238228d00e5c210da701fed7def9c8f
piwe2004 <piwe2004@gmail.com>
2026-01-19 15:26:08 +0900

    목업 데이터 관련 작업

--- src/types/places.ts
+++ src/types/places.ts
@@ -1,3 +1,61 @@
+export interface LodgingDetail {
+  contentid: string;
+  contenttypeid: string;
+  roomcount: string;
+  roomtype: string;
+  refundregulation: string;
+  checkintime: string;
+  checkouttime: string;
+  chkcooking: string;
+  seminar: string;
+  sports: string;
+  sauna: string;
+  beauty: string;
+  beverage: string;
+  karaoke: string;
+  barbecue: string;
+  campfire: string;
+  bicycle: string;
+  fitness: string;
+  publicpc: string;
+  publicbath: string;
+  subfacility: string;
+  foodplace: string;
+  reservationurl: string;
+  pickup: string;
+  infocenterlodging: string;
+  parkinglodging: string;
+  reservationlodging: string;
+  scalelodging: string;
+  accomcountlodging: string;
+}
+
+export interface AttractionDetail {
+  contentid: string;
+  contenttypeid: string;
+  infocenter: string;
+  restdate: string;
+  usetime: string;
+  parking: string;
+  chkbabycarriage: string;
+  chkpet: string;
+  chkcreditcard: string;
+}
+
+export interface RestaurantDetail {
+  contentid: string;
+  contenttypeid: string;
+  firstmenu: string;
+  treatmenu: string;
+  smoking: string;
+  packing: string;
+  infocenterfood: string;
+  opentimefood: string;
+  restdatefood: string;
+  parkingfood: string;
+  reservationfood: string;
+}
+
 export interface FirebasePlace {
   PLACE_ID: number;
   NAME: string;
@@ -24,4 +82,6 @@ export interface FirebasePlace {
     areaCd: string;
     sigeCd: string; // 시군구 코드 (필수)
   };
+  // Detailed information based on category
+  detail?: LodgingDetail | AttractionDetail | RestaurantDetail;
 }
